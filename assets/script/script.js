d0d8a1392d2183ab44888e010396fb527bff84e5
ismailcankaratasss@gmail.com <ismailcankaratasss@gmail.com>
2023-02-16 20:46:54 +0300

    bug fixed

--- assets/script/script.js
+++ assets/script/script.js
@@ -11,28 +11,24 @@ function setGreenColor() { // Burak Talha Tarı
     // Oyun alanı arka planı #4bdb6a (yeşil) renk kodu ile değiştir.
     game.style.backgroundColor = "#4bdb6a";
     // Ekrana "Click" yaz.
-    title.innerHTML="Click"
-    description.innerHTML=""
-    icon.innerHTML='<svg aria-hidden="true" widht="100" height="100" focusable="false" data-prefix="fas" data-icon="ellipsis-h" class="svg-inline--fa fa-ellipsis-h fa-w-16 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M328 256c0 39.8-32.2 72-72 72s-72-32.2-72-72 32.2-72 72-72 72 32.2 72 72zm104-72c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72zm-352 0c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72z"></path></svg>'
+    title.innerHTML = "Click"
+    description.innerHTML = ""
+    icon.innerHTML = '<svg aria-hidden="true" widht="100" height="100" focusable="false" data-prefix="fas" data-icon="ellipsis-h" class="svg-inline--fa fa-ellipsis-h fa-w-16 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M328 256c0 39.8-32.2 72-72 72s-72-32.2-72-72 32.2-72 72-72 72 32.2 72 72zm104-72c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72zm-352 0c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72z"></path></svg>'
     // "timeNow" değişkenine şimdi ki zamanı milisaniye cinsinden ata.
-    var timeNow = Date.now();
+    timeNow = Date.now();
     greenDisplayed = true;
 }
-
-
-
 
 function startGame() { // Alper Dağdelen
     //(Yapıldı) Oyun alanına tıklandığı zaman arkaplan rengi #ce2636 (kırmızı) renk kodu ile değiştir.
     // (Yapıldı)Ekrana "Wait for green" yaz.
-    
-        game.style.background="#ce2636";
-            title.innerHTML="Wait For Green..."
-            description.innerHTML=""
-            icon.innerHTML='<svg width="100" height="128" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="ellipsis-h" class="svg-inline--fa fa-ellipsis-h fa-w-16 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M328 256c0 39.8-32.2 72-72 72s-72-32.2-72-72 32.2-72 72-72 72 32.2 72 72zm104-72c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72zm-352 0c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72z"></path></svg>'
-            let randoms=Math.floor(Math.random()*8000);   
-            const delay=setTimeout(setGreenColor,randoms);
-            console.log(randoms)
+
+    game.style.background = "#ce2636";
+    title.innerHTML = "Wait For Green..."
+    description.innerHTML = ""
+    icon.innerHTML = '<svg width="100" height="128" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="ellipsis-h" class="svg-inline--fa fa-ellipsis-h fa-w-16 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M328 256c0 39.8-32.2 72-72 72s-72-32.2-72-72 32.2-72 72-72 72 32.2 72 72zm104-72c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72zm-352 0c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72z"></path></svg>'
+    let randoms = Math.floor(Math.random() * 8000) + 1000;
+    setTimeout(setGreenColor, randoms);
     // (Yapıldı)1000 ile 8000 arasında random sayı üret (1 saniye ile 8 saniye arası)
     // (Yapıldı)Zaman aşımı oluştur. Üretilen saniye kadar bekleyip setGreenColor function 'ı çalıştırsın.
 }
@@ -42,16 +38,14 @@ function displayReactionTime() { // Ege Sezer
     // ekrana reactionTime değerini yazdır
     greenDisplayed = false;
     game.style.backgroundColor = "#2b87d1";
-    title.innerHTML=reactionTime +  " ms ";
-    description.innerHTML="";
-    icon.innerHTML="";
-    
+    title.innerHTML = reactionTime + " ms ";
+    description.innerHTML = "";
+    icon.innerHTML = "";
 }
-
 
 game.addEventListener("click", function () {
     if (greenDisplayed) {
-        reactionTime = (new Date.now() - timeNow);  56
+        reactionTime = (Date.now() - timeNow);
         displayReactionTime();
     } else {
         startGame();
